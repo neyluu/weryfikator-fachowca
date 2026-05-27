eafff53208b357b5159a3b2fe2c73f87c36dd5f0
neyluu <75064225+neyluu@users.noreply.github.com>
2026-05-27 16:37:19 +0200

    Added form validation

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -16,6 +16,7 @@ function Profile() {
     const saved = localStorage.getItem("profileCreation");
     return saved ? JSON.parse(saved) : false;
   });
+  const [errorMessage, setErrorMessage] = useState("");
 
   const [formData, setFormData] = useState({
     specialization: "",
@@ -75,7 +76,7 @@ function Profile() {
     { key: "dietetics", label: "Dietetyka" },
     { key: "gastronomy", label: "Gastronomia" },
     { key: "gardening", label: "Ogrodnictwo" },
-  ]
+  ];
 
   const [priceTypes, setPriceTypes] = useState({
     consultation: {
@@ -188,7 +189,110 @@ function Profile() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+
+    const res = validateForm();
+    if (!res.success) {
+      setErrorMessage(res.message)
+    }
+
+    // api
+  };
+
+  const validateForm = () => {
+    if (!formData.specialization.trim()) {
+      return {
+        success: false,
+        message: "Specjalizacja jest wymagana.",
+      };
+    }
+
+    if (!formData.description.trim()) {
+      return {
+        success: false,
+        message: "Opis jest wymagany.",
+      };
+    }
+
+    if (!formData.experience.trim()) {
+      return {
+        success: false,
+        message: "Doświadczenie jest wymagane.",
+      };
+    }
+
+    if (!formData.localization.trim()) {
+      return {
+        success: false,
+        message: "Lokalizacja jest wymagana.",
+      };
+    }
+
+    if (!formData.phoneNumber.trim()) {
+      return {
+        success: false,
+        message: "Numer telefonu jest wymagany.",
+      };
+    }
+
+    if (!formData.email.trim()) {
+      return {
+        success: false,
+        message: "Adres e-mail jest wymagany.",
+      };
+    }
+
+    const phoneRegex = /^(\+48)?[\s-]?(\d{3}[\s-]?\d{3}[\s-]?\d{3})$/;
+
+    if (!phoneRegex.test(formData.phoneNumber)) {
+      return {
+        success: false,
+        message: "Numer telefonu ma nieprawidłowy format.",
+      };
+    }
+
+    const hasEnabledPrice = Object.values(formData.prices).some(
+      (price) => price.enabled,
+    );
+
+    if (!hasEnabledPrice) {
+      return {
+        success: false,
+        message: "Musisz wybrać przynajmniej jeden rodzaj wyceny.",
+      };
+    }
+
+    if (formData.images.length < 1 || formData.images.length > 8) {
+      return {
+        success: false,
+        message: "Liczba zdjęć musi być między 1 a 8.",
+      };
+    }
+
+    if (formData.hourStart >= formData.hourEnd) {
+      return {
+        success: false,
+        message:
+          "Godzina rozpoczęcia musi być wcześniejsza niż godzina zakończenia.",
+      };
+    }
+
+    if (!formData.days.length) {
+      return {
+        success: false,
+        message: "Musisz wybrać przynajmniej jeden dzień.",
+      };
+    }
+
+    if (formData.categories.length < 1 || formData.categories.length > 5) {
+      return {
+        success: false,
+        message: "Liczba kategorii musi być między 1 a 5.",
+      };
+    }
+
+    return {
+      success: true,
+    };
   };
 
   useEffect(() => {
@@ -512,7 +616,7 @@ function Profile() {
               </label>
 
               <div className="flex flex-wrap gap-2 w-full">
-                {DAYS.map(({key, label}) => {
+                {DAYS.map(({ key, label }) => {
                   const active = formData.days.includes(key);
 
                   return (
@@ -546,6 +650,12 @@ function Profile() {
               </Button>
             </div>
           </form>
+
+          {errorMessage && (
+            <p className="flex justify-center text-red-600 border-red-300 border-2 rounded-3xl p-2 bg-red-100">
+              {errorMessage}
+            </p>
+          )}
         </div>
       )}
     </div>
