77679895eefb3e02373382d704499ce77fb160e3
Albert G. <252793@edu.p.lodz.pl>
2026-06-23 21:15:32 +0200

    Fixed JWT parser to work properly with polish characters

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,7 +3,15 @@ import { useNavigate } from "react-router-dom";
 const AuthContext = createContext();
 function parseJwt(token) {
   try {
-    return JSON.parse(atob(token.split(".")[1]));
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split("")
+        .map((c) => "%" + c.charCodeAt(0).toString(16).padStart(2, "0"))
+        .join(""),
+    );
+    return JSON.parse(jsonPayload);
   } catch {
     return null;
   }
